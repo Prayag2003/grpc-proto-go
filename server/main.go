2d6c6ef5425d9c31846aad3639e97a25a4d1fc0e
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-09-07 23:04:22 +0530

    [feat-server]: implement basic gRPC server setup

--- server/main.go
+++ server/main.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+}
