5f8a725ad74783f9edda3de97e484c3ec1eeea4b
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-09-08 23:21:05 +0530

    [feat-client]: implement initial gRPC client connection setup

--- client/main.go
+++ client/main.go
@@ -1,1 +1,28 @@
 package client
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	PORT = ":8080"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost"+PORT, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("couldn't connect: %v", err)
+	}
+	defer conn.Close()
+
+	// client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NamesList{
+	// 	Names: []string{"A", "B", "C"},
+	// }
+
+	// callSayHello(client)
+}
