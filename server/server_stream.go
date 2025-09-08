5e25c7b9fce0a739bf33c4c313925057334301ae
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-09-08 23:41:46 +0530

    [feat-server]: add sayHelloServerStreaming method for server-side streaming

---
+++ server/server_stream.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	pb "github.com/Prayag2003/grpc-proto-go/proto"
+)
+
+func (s *helloServer) sayHelloServerStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloFromServerStreamingServer) error {
+	log.Printf("Got request for names: %v", req)
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello " + name,
+		}
+		if err := stream.Send(res); err != nil {
+			return err
+		}
+		time.Sleep(2 * time.Second)
+	}
+	return nil
+}
